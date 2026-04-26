608b31654e81b3abe91fd0ea1e2453d547daadbc
samiul-24011 <it24011@mbstu.ac.bd>
2026-04-26 10:49:58 +0600

    Create IT24011.java

---
+++ IT24011.java
@@ -0,0 +1,6 @@
+public class IT24011 { //Create another class
+        public static void main(String[] args) {
+                Vehicle v = new Vehicle(args[0], args[1]);
+                v.start();
+        }
+}
